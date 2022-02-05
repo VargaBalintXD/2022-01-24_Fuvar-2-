b707067b12a81d29de0445f992474e36e4db757e
VargaBalintXD <vargab.12d@acsjszki.hu>
2022-02-05 16:35:42 +0100

    class + int+double

--- 2022-01-24_Fuvar/Program.cs
+++ 2022-01-24_Fuvar/Program.cs
@@ -7,6 +7,23 @@ using System.IO;
 
 namespace _2022_01_24_Fuvar
 {
+	class Fuvar {
+		public int taxiAzonosito { get; set; }
+		public string utazasIdopontja { get; set; }
+		public int utazasIdotartama { get; set; }
+		public double megtettTavolsag { get; set; }
+		public double viteldij { get; set; }
+		public double borravalo { get; set; }
+		public string fizetesMod { get; set; }
+		public Fuvar(string s)
+		{ 
+		
+		
+		
+		
+		}
+
+		}
 	class Program
 	{
 		static void Main(string[] args)
